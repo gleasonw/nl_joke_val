959bdbd7675791bf156c067e10f40295f8964e2a
Will <wgleason9@gmail.com>
2023-04-19 09:19:38 -0700

    post clips for segment reads

--- api/api.go
+++ api/api.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
@@ -24,9 +27,34 @@ type SeriesData struct {
 	Time   float64 `json:"time"`
 }
 
+var auth_token string = os.Getenv("AUTH_TOKEN")
+var nickname string = os.Getenv("NICK")
+var db_url string = os.Getenv("DATABASE_URL")
+var client_id string = os.Getenv("CLIENT_ID")
+
 func main() {
-	connStr := os.Getenv("DATABASE_URL")
-	db, err := sql.Open("postgres", connStr)
+	if auth_token == "" {
+		//load .env file
+		fmt.Println("local")
+		err := godotenv.Load()
+		if err != nil {
+			fmt.Println("Error loading .env file")
+		}
+		auth_token = os.Getenv("AUTH_TOKEN")
+		nickname = os.Getenv("NICK")
+		db_url = os.Getenv("DATABASE_URL")
+		client_id = os.Getenv("CLIENT_ID")
+	}
+	if db_url == "" {
+		//load .env file
+		err := godotenv.Load()
+		if err != nil {
+			fmt.Println("Error loading .env file")
+		}
+		db_url = os.Getenv("DATABASE_URL")
+	}
+
+	db, err := sql.Open("postgres", db_url)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -43,7 +71,6 @@ func main() {
 		}
 		marshal_json_and_write(w, total)
 	})
-	// hello route
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!"))
 	})
@@ -85,16 +112,16 @@ func main() {
 		span := r.URL.Query().Get("span")
 		grouping := r.URL.Query().Get("grouping")
 		rows, err := db.Query(`
-			SELECT SUM(count) OVER(ORDER BY created), 
-				SUM(lol) OVER (ORDER BY created), 
-				SUM(cereal) OVER (ORDER BY created), 
-				SUM(monkas) OVER (ORDER BY created), 
-				SUM(joel) OVER (ORDER BY created), 
-				SUM(pogs) OVER (ORDER BY created), 
-				SUM(huhs) OVER (ORDER BY created),
-				EXTRACT(epoch from date_trunc($1, created)) AS created_epoch
-			FROM counts 
-			WHERE created > NOW() - $2::interval
+			SELECT SUM(count) OVER (ORDER BY created RANGE INTERVAL $1::interval PRECEDING),
+				SUM(lol) OVER (ORDER BY created RANGE INTERVAL $1::interval PRECEDING),
+				SUM(cereal) OVER (ORDER BY created RANGE INTERVAL $1::interval PRECEDING),
+				SUM(monkas) OVER (ORDER BY created RANGE INTERVAL $1::interval PRECEDING),
+				SUM(joel) OVER (ORDER BY created RANGE INTERVAL $1::interval PRECEDING),
+				SUM(pogs) OVER (ORDER BY created RANGE INTERVAL $1::interval PRECEDING),
+				SUM(huhs) OVER (ORDER BY created RANGE INTERVAL $1::interval PRECEDING),
+			EXTRACT(epoch from date_trunc('minute', created)) AS created_epoch
+ 			FROM counts
+ 			WHERE created > NOW() - $2::interval; 
 		`, grouping, span)
 		if err != nil {
 			fmt.Println(err)
@@ -113,27 +140,27 @@ func main() {
 		}
 		marshal_json_and_write(w, data)
 	})
-	// http.HandleFunc("/api/max", func(w http.ResponseWriter, r *http.Request) {
-	// 	var max int
-	// 	var time float64
-	// 	db.QueryRow("SELECT count, EXTRACT(epoch from created) FROM counts WHERE count=(SELECT max(count) from counts)").Scan(&max, &time)
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 		return
-	// 	}
-	// 	marshal_json_and_write(w, SeriesData{Y: max, X: time})
-	// })
-	// http.HandleFunc("/api/min", func(w http.ResponseWriter, r *http.Request) {
-	// 	var min int
-	// 	var time float64
-	// 	db.QueryRow("SELECT count, EXTRACT(epoch from created) FROM counts WHERE count=(SELECT min(count) from counts)").Scan(&min, &time)
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 		return
-	// 	}
-	// 	marshal_json_and_write(w, SeriesData{Y: min, X: time})
+	http.HandleFunc("/api/max", func(w http.ResponseWriter, r *http.Request) {
+		var max int
+		var time float64
+		err := db.QueryRow("SELECT count, EXTRACT(epoch from created) FROM counts WHERE count=(SELECT max(count) from counts)").Scan(&max, &time)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		marshal_json_and_write(w, SeriesData{Twos: max, Time: time})
+	})
+	http.HandleFunc("/api/min", func(w http.ResponseWriter, r *http.Request) {
+		var min int
+		var time float64
+		err := db.QueryRow("SELECT count, EXTRACT(epoch from created) FROM counts WHERE count=(SELECT min(count) from counts)").Scan(&min, &time)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		marshal_json_and_write(w, SeriesData{Twos: min, Time: time})
 
-	// })
+	})
 
 	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
 	fmt.Println("Listening on port", port)
@@ -184,10 +211,9 @@ type ChatCounts struct {
 }
 
 func read_chat(conn *websocket.Conn, chat_closed chan error, db *sql.DB) {
-	auth := os.Getenv("AUTH_TOKEN")
-	nick := os.Getenv("NICK")
-	conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("PASS %s", auth)))
-	conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("NICK %s", nick)))
+
+	conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("PASS %s", auth_token)))
+	conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("NICK %s", nickname)))
 	conn.WriteMessage(websocket.TextMessage, []byte("JOIN #northernlion"))
 
 	incomingMessages := make(chan Message)
@@ -227,7 +253,7 @@ func read_chat(conn *websocket.Conn, chat_closed chan error, db *sql.DB) {
 			if strings.Contains(only_message_text, "Joel") {
 				counter.Joels++
 			}
-			if strings.Contains(only_message_text, "POGCRAZY") {
+			if strings.Contains(only_message_text, "POGCRAZY") || strings.Contains(only_message_text, "Pog") {
 				counter.PogCrazies++
 			}
 			if strings.Contains(only_message_text, "HUHH") {
@@ -241,7 +267,12 @@ func read_chat(conn *websocket.Conn, chat_closed chan error, db *sql.DB) {
 				counter.Twos -= delta
 			}
 		case <-post_count_ticker.C:
-			db.Exec("INSERT INTO counts (count, lol, cereal, monkas, joel, pogs, huhs) VALUES ($1, $2, $3, $4, $5, $6, $7)", counter.Twos, counter.LulsAndICANTS, counter.Cereals, counter.Monkas, counter.Joels, counter.PogCrazies, counter.Huhs)
+			var timestamp time.Time
+			err := db.QueryRow("INSERT INTO counts (count, lol, cereal, monkas, joel, pogs, huhs) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING created", counter.Twos, counter.LulsAndICANTS, counter.Cereals, counter.Monkas, counter.Joels, counter.PogCrazies, counter.Huhs).Scan(&timestamp)
+			if err != nil {
+				fmt.Println("Error inserting into db:", err)
+			}
+			go create_clip(db, timestamp)
 			counter = ChatCounts{}
 		}
 	}
@@ -261,6 +292,68 @@ func parse_val(text string) int {
 		}
 	}
 	return 0
+}
+
+func create_clip(db *sql.DB, unix_timestamp time.Time) {
+	requestBody := map[string]string{
+		"broadcaster_id": "14371185",
+		"has_delay":      "false",
+	}
+
+	// Convert the request body to a JSON string
+	requestBodyBytes, err := json.Marshal(requestBody)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+
+	// Create a new HTTP POST request to the Twitch Clip API
+	req, err := http.NewRequest("POST", "https://api.twitch.tv/helix/clips", bytes.NewBuffer(requestBodyBytes))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	auth := split_and_get_last(auth_token, ":")
+	bearer := fmt.Sprintf("Bearer %s", auth)
+
+	// Set the required headers for the request
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Client-Id", client_id)
+	req.Header.Set("Authorization", bearer)
+
+	// Send the request and get the response
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	// get clip_id from json response
+	var responseObject map[string]interface{}
+	err = json.Unmarshal(responseBody, &responseObject)
+	if err != nil {
+		fmt.Println("Error unmarshaling JSON:", err)
+		return
+	}
+	data, ok := responseObject["data"].([]interface{})
+	if !ok {
+		return
+	}
+	clip_id, ok := data[0].(map[string]interface{})["id"].(string)
+	if !ok {
+		return
+	}
+	db.Exec("UPDATE counts SET clip_id = $1 WHERE created = $2", clip_id, unix_timestamp)
 }
 
 type Message struct {
