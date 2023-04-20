5206ab502db4ffca1c0225367d858deb9361ce5a
Will <wgleason9@gmail.com>
2023-04-20 10:17:32 -0700

    date fix

--- front/app.vue
+++ front/app.vue
@@ -11,9 +11,9 @@ interface SeriesData {
     time: number;
 }
 
-const series_calc = ref < 'rolling_sum' | 'instant' > ('instant');
-const span = ref < '1 minute' | '5 minutes' | '30 minutes' | '1 hour' | '1 day' | '1 week' | '1 month' | '1 year' > ('30 minutes');
-const grouping = ref < 'second' | 'minute' | 'hour' | 'day' | 'week' | 'month' | 'year' > ('minute');
+const series_calc = ref<'rolling_sum' | 'instant'>('instant');
+const span = ref<'1 minute' | '5 minutes' | '30 minutes' | '1 hour' | '1 day' | '1 week' | '1 month' | '1 year'>('30 minutes');
+const grouping = ref<'second' | 'minute' | 'hour' | 'day' | 'week' | 'month' | 'year'>('minute');
 const currentTime = ref(new Date().getTime());
 const clickedUnixSeconds = ref(Date.now() / 1000);
 const lineChart = ref();
@@ -22,20 +22,20 @@ const url = computed(() => `https://nljokeval-production.up.railway.app/api/${se
 
 const {
     data
-} = await useFetch < SeriesData[] > (url);
+} = await useFetch<SeriesData[]>(url);
 const keys = computed(() => data.value && data.value.length > 0 && Object.keys(data.value?.[0]).filter(k => k !== "time") || ([] as string[]));
 const selectedKeys = ref(new Set(["twos"]))
 const nuxtApp = useNuxtApp();
 const Chart = nuxtApp.$Chart;
 
 const seriesColors = {
-  twos: "#7cb5ec",
-  lol: "#434348",
-  cereal: "#90ed7d",
-  monkas: "#f7a35c",
-  joel: "#8085e9",
-  pogs: "#f15c80",
-  huhs: "#e4d354",
+    twos: "#7cb5ec",
+    lol: "#434348",
+    cereal: "#90ed7d",
+    monkas: "#f7a35c",
+    joel: "#8085e9",
+    pogs: "#f15c80",
+    huhs: "#e4d354",
 };
 
 // "#2b908f",
@@ -60,11 +60,11 @@ const chartOptions = computed((): Highcharts.Options => ({
         height: 600,
         zooming: {
             type: "x",
-        }        ,
+        },
         events: {
             click: function (e: any) {
                 const xVal = e?.xAxis?.[0]?.value;
-                if(xVal) {
+                if (xVal) {
                     clickedUnixSeconds.value = Math.round(xVal / 1000);
                 }
             },
@@ -96,7 +96,6 @@ const chartOptions = computed((): Highcharts.Options => ({
         color: seriesColors[key as keyof typeof seriesColors],
         events: {
             click: function (e: any) {
-                console.log(e.point.x);
                 clickedUnixSeconds.value = e.point.x / 1000;
             },
         },
@@ -116,30 +115,60 @@ setInterval(() => {
 </script>
 
 <template>
-<select v-model="series_calc">
-    <option value="rolling_sum">rolling_sum</option>
-    <option value="instant">instant</option>
-</select>
-<select v-model="span">
-    <option value="1 minute">1 minute</option>
-    <option value="5 minutes">5 minutes</option>
-    <option value="30 minutes">30 minutes</option>
-    <option value="1 hour">1 hour</option>
-    <option value="1 day">1 day</option>
-    <option value="1 week">1 week</option>
-    <option value="1 month">1 month</option>
-    <option value="1 year">1 year</option>
-</select>
-<select v-model="grouping">
-    <option value="second">second</option>
-    <option value="minute">minute</option>
-    <option value="hour">hour</option>
-    <option value="day">day</option>
-    <option value="week">week</option>
-    <option value="month">month</option>
-    <option value="year">year</option>
-</select>
-<button v-for="key in keys" @click="() => handleSeriesButton(key)">{{key}}</button>
-<Chart :options="chartOptions" ref="lineChart" />
-<ClipViewer :time="clickedUnixSeconds" />
+    <div class="flex-row">
+        <label for="calc">Calc</label>
+        <select v-model="series_calc" id="calc">
+            <option value="rolling_sum">rolling_sum</option>
+            <option value="instant">instant</option>
+        </select>
+        <label for="past">Past</label>
+        <select v-model="span" id="past">
+            <option value="1 minute">1 minute</option>
+            <option value="5 minutes">5 minutes</option>
+            <option value="30 minutes">30 minutes</option>
+            <option value="1 hour">1 hour</option>
+            <option value="1 day">1 day</option>
+            <option value="1 week">1 week</option>
+            <option value="1 month">1 month</option>
+            <option value="1 year">1 year</option>
+        </select>
+        <label for="grouping">Group by</label>
+        <select v-model="grouping" id="grouping">
+            <option value="second">second</option>
+            <option value="minute">minute</option>
+            <option value="hour">hour</option>
+            <option value="day">day</option>
+            <option value="week">week</option>
+            <option value="month">month</option>
+            <option value="year">year</option>
+        </select>
+
+        <button v-for="key in keys" @click="() => handleSeriesButton(key)"
+            :class="{ faded: !selectedKeys.has(key), 'series-button': true }"
+            :style="{ backgroundColor: seriesColors[key as keyof typeof seriesColors] }">
+        {{ key }}
+        </button>
+    </div>
+    <Chart :options="chartOptions" ref="lineChart" />
+    <ClipViewer :time="clickedUnixSeconds" />
 </template>
+
+<style>
+.faded {
+    opacity: 0.5;
+}
+
+.series-button {
+    padding: 5px;
+    background-color: white;
+}
+
+.flex-row {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 10px;
+    align-items: center;
+    justify-content: center;
+}
+</style>
--- front/components/ClipViewer.vue
+++ front/components/ClipViewer.vue
@@ -14,11 +14,13 @@ onMounted(() => {
 const {
     data
 } = await useFetch<ClipData>(computed(() => `https://nljokeval-production.up.railway.app/api/clip?time=${props.time}`));
-console.log(data.value);
 const vidSource = computed(() => `https://clips.twitch.tv/embed?clip=${data.value?.clip_id}&parent=${parent.value}`);
+console.log(data.value);
+
 </script>
 
 <template>
-    <p v-if="data">{{ new Date(data.time).toLocaleString() }}</p>
-<iframe :src="vidSource" v-if="vidSource && parent" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620" />
+    <p v-if="data && data.time > 0">{{ new Date(data.time * 1000).toLocaleString() }}</p>
+    <iframe :src="vidSource" v-if="vidSource && parent" frameborder="0" allowfullscreen="true" scrolling="no" height="378"
+        width="620" />
 </template>
