0aeb53fd5656f95402fad30c454d68b147b217e7
Will <wgleason9@gmail.com>
2023-04-20 08:21:33 -0700

    iframe

--- front/components/ClipViewer.vue
+++ front/components/ClipViewer.vue
@@ -2,14 +2,18 @@
 const props = defineProps({
     time: Number
 })
+const parent = ref('');
+onMounted(() => {
+    parent.value = window.location.hostname;
+})
 
 const {
     data
 } = await useFetch(computed(() => `https://nljokeval-production.up.railway.app/api/clip?time=${props.time}`));
 
-const vidSource = computed(() => `https://clips.twitch.tv/embed?clip=${data.value}&parent=localhost`);
+const vidSource = computed(() => `https://clips.twitch.tv/embed?clip=${data.value}&parent=${parent.value}`);
 </script>
 
 <template>
-<iframe :src="vidSource" v-if="vidSource" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620" />
+<iframe :src="vidSource" v-if="vidSource && parent" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620" />
 </template>
