449ca57864219c9c71998800974ac4d7d7324af3
Will Gleason <wgleason9@gmail.com>
2023-06-21 20:59:21 +0200

    Update Dashboard.tsx

--- new-front/src/components/Dashboard.tsx
+++ new-front/src/components/Dashboard.tsx
@@ -407,6 +407,7 @@ function TwitchClip({ clip_id, time }: { clip_id: string; time: number }) {
         width="100%"
         className="aspect-video max-w-2xl"
         allowFullScreen={true}
+        preload="none"
       />
     </>
   );
