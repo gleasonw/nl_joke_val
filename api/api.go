f9aee491777b9dc4cd527cff5a55613d87c817fa
Will Gleason <will.gleason@novadiscovery.com>
2023-09-30 20:12:25 +0200

    observe

--- api/api.go
+++ api/api.go
@@ -504,6 +504,7 @@ func connectToTwitchChat(db *gorm.DB) {
 	chat_closed := make(chan error)
 
 	go func() {
+		fmt.Println(authToken, nickname)
 		conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("PASS %s", authToken)))
 		conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("NICK %s", nickname)))
 		conn.WriteMessage(websocket.TextMessage, []byte("JOIN #northernlion"))
